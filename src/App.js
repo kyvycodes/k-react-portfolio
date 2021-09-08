0ae9a4f7c762d627aa11ec51c44374ba9652ae7d
Kay <kyv.hardeman@gmail.com>
2021-09-08 18:19:06 -0400

    feature: navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import NavMenu from './components/NavMenu';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-      <h1>Welcome to react portfolio Project</h1>
-      <h3></h3>
+    <Router>
+      <NavMenu/>
+    </Router>
     </>
   );
 }
---
+++ src/components/NavMenu.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function NavMenu() {
+  return(
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+          <NavLink to="/projects">Projects</NavLink>
+          </li>
+          <li>
+          <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+          <NavLink to="/passions">Passions</NavLink>
+          </li>
+        {/* <li>Projects</li> */}
+        {/* <li>Contact</li>
+        <li>Passions</li> */}
+      </ul>
+    </div>
+  )
+}
