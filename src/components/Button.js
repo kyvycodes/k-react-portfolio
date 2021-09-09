35b72eeda3b00eab0f4e69be1b0add8dab2e1c54
Kay <kyv.hardeman@gmail.com>
2021-09-09 19:58:53 -0400

    feat: hero section styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,44 @@
 import React from 'react'
 import { Link, BrowserRouter } from 'react-router-dom'
+import styled from 'styled-components'
+
+  //if the outline is true the background color will be transparent otherwise it will be gray
+//  background-color: ${(props) =>
+//   props.outline ? 'transparent' : 'var(--gray-1)'
+//   }
+// background-color: ${(props) =>
+//   props.outline ? 'transperant' : 'var(--gray-1)'};
+const ButtonStyle = styled.div`
+margin-top: 2rem;
+.button {
+  font-size: 2.4rem;
+  padding: .7em 2em;
+  border-radius: 8px;
+  display: inline-block;
+  //TODO come back look up correct () syntax for arrow functions
+  color: ${props =>
+    props.outline ? 'var(--gray-1)' : 'purple'
+    };
+   background-color: ${(props) =>
+    props.outline ? 'yellow' : 'var(--gray-1)'
+    };
+  opacity: 0.5;;
+  border: 2px solid var(--gray-1)
+}
+@media only screen and (max-width: 768px){
+  .button{
+    font-size: 1.8rem;
+  }
+}
+`
 
 export default function Button(props) {
   console.log('btn', props)
   return (
-    <div>
+    <ButtonStyle outline={props.outline}>
       <BrowserRouter>
         <Link className='button' to={props.btnLink}>{props.btnText}</Link>
       </BrowserRouter>
-    </div>
+    </ButtonStyle>
   )
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -17,7 +17,8 @@ export default function HeroSection() {
           <div className='hero_info'>
             <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. Gingerbread sugar plum dessert marzipan bear claw jelly beans cake brownie. Donut caramels donut donut tart carrot cake powder carrot cake bonbon.</ParagraphText>
           </div>
-          <Button btnLink ='/projects' btnText='View My Work 🌸' />
+          <Button btnLink ='/projects' btnText='View My Work 🌸' outline={false}/>
+          {/* props created by me: btnLink, btnText, outline - can be named whatever and can have multiple datatypes */}
       </div>
     </div>
   )
