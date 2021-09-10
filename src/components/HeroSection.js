3030e40252aafcb96bc47c1106ff2680ac0bf841
Kay <kyv.hardeman@gmail.com>
2021-09-10 09:42:21 -0400

    bug: hero text overlap

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -41,6 +41,16 @@ const HeroStyles = styled.div`
   margin: 0 auto;
   border: 2px solid var(--gray-1);
 }
+.hero_info {
+  margin-top: -20rem;
+}
+.hero_interests,
+.hero_scroll_down{
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  position: absolute;
+}
 `
 
 
@@ -58,7 +68,7 @@ export default function HeroSection() {
             <img src={ DoorImg } />
           </div>
           <div className='hero_info'>
-            <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. Gingerbread sugar plum dessert marzipan bear claw jelly beans cake brownie. Donut caramels donut donut tart carrot cake powder carrot cake bonbon.</ParagraphText>
+            <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. Donut caramels donut donut tart carrot cake powder carrot cake bonbon.</ParagraphText>
           </div>
           <Button btnLink ='/projects' btnText='View My Work 🌸' outline={false}/>
           {/* props created by me: btnLink, btnText, outline - can be named whatever and can have multiple datatypes */}
@@ -76,12 +86,24 @@ export default function HeroSection() {
                   rel="noreferrer">
                   LI
                   </a>
+                  <a href="https://linkedin.com/in/kay-hardeman/"
+                  // opens new browser window
+                  target='_blank'
+                  rel="noreferrer">
+                  LI
+                  </a>
+                  <a href="https://linkedin.com/in/kay-hardeman/"
+                  // opens new browser window
+                  target='_blank'
+                  rel="noreferrer">
+                  LI
+                  </a>
                 </li>
               </ul>
             </div>
         </div>
       </div>
-      <div className="hero_scrolldown">
+      <div className="hero_scroll_down">
       <p>scroll</p>
       {/* <img src={ScrollDownArrow} alt=''/> */}
       </div>
