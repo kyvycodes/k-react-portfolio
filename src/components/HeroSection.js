e497a5f0396a970fa52b8a96000eb8975b80d817
Kay <kyv.hardeman@gmail.com>
2021-09-10 21:31:32 -0400

    feat: full hero section css

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,10 +2,10 @@ import React from 'react'
 import DoorImg from '../assets/images/door.jpg'
 import Button from './Button'
 import ParagraphText from './ParagraphText'
-import InterestsArrow from '../assets/images/interests-arrow.svg'
-import ScrollDownArrow from '../assets/images/scroll-down-arrow.svg'
 import styled from 'styled-components'
 import Hero from '../assets/images/hero.png'
+import Interests from '../assets/images/interests-arrow.svg'
+import ScrollDown from '../assets/images/scroll-down-arrow.svg'
 
 const HeroStyles = styled.div`
 .hero_section{
@@ -23,43 +23,37 @@ const HeroStyles = styled.div`
   font-size: 1.5rem;
   margin-bottom: -5.5rem;
   position: relative;
-  color: red;
   span {
     display: inline-block;
     width: 100%;
-    color: red;
+    color: var(--white)
   }
   .hero_name {
     font-size: 6rem;
     //TODO change font
     // font-family:
-    // color: var(--white)
-    color: red;
+    color: var(--white)
   }
 }
 .door_img {
-  // max-width: 900px;
+  max-width: 900px;
   width: 100%;
   height: 600px;
   margin: 0 auto;
   border: 2px solid var(--gray-1);
 }
 
-
 .hero_info {
-  color: red;
   margin-top: -20rem;
 
 }
 
-//#FIX ME - I'm overlapping
 .hero_interests,
 .hero_scroll_down {
   display: flex;
   flex-direction: column;
   gap: 2rem;
   position: absolute;
-  color: red;
   bottom: 20px;
   width: 50px;
 }
@@ -70,6 +64,47 @@ const HeroStyles = styled.div`
 .hero_scroll_down {
   right: 50px;
 }
+
+.hero_interests_indicator,
+.hero_scroll_down {
+  width: 50px;
+  p {
+    font-size: 1.6rem;
+    transform: translateY(-70px) rotate(90deg); //rotates text
+    letter-spacing: .7rem; //increases visibility
+    text-transform: uppercase;
+  }
+  img{
+    max-height: 45px;
+    width: 16px;
+    margin: 0 auto;
+    object-fit: contain;
+  }
+}
+.hero_scroll_down {
+//   p {
+//     transform: translateY(-70px) rotate(90deg); //rotates text
+//  }
+    img {
+    max-height: 70px;
+  }
+}
+
+.hero_interests_text {
+  ul {
+    li {
+      margin-bottom: 1rem;
+    }
+    a {
+      display: inline-block;
+      font: 1.4rem;
+      transform: rotate(-90deg);
+      // letter-spacing: 5px;
+      margin-bottom: 1.5rem;
+    }
+  }
+}
+
 `
 
 
@@ -83,18 +118,17 @@ export default function HeroSection() {
           <span className="hero_name">Kay Hardeman</span>
           </h1>
           <div className="door_img">
-            {/* <img src={DoorImg} alt=''/> */}
-            <img src={ DoorImg } />
+            <img src={DoorImg} alt=''/>
           </div>
           <div className="hero_info">
-            <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. Donut caramels donut donut tart carrot cake powder carrot cake bonbon.</ParagraphText>
+            <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. </ParagraphText>
             <Button btnLink ='/projects' btnText='View My Work 🌸' outline={false}/>
           {/* props created by me: btnLink, btnText, outline - can be named whatever and can have multiple datatypes */}
           </div>
       <div className='hero_interests'>
           <div className='hero_interests_indicator'>
             <p>customize me</p>
-            {/* <img src={InterestsArrow} alt=''/> */}
+            <img src={Interests} alt=''/>
             </div>
             <div className='hero_interests_text'>
               <ul>
@@ -109,7 +143,6 @@ export default function HeroSection() {
                   </li>
                   <li>
                   <a href="https://linkedin.com/in/kay-hardeman/"
-                  // opens new browser window
                   target='_blank'
                   rel="noreferrer">
                   LI
@@ -117,7 +150,6 @@ export default function HeroSection() {
                   </li>
                   <li>
                   <a href="https://linkedin.com/in/kay-hardeman/"
-                  // opens new browser window
                   target='_blank'
                   rel="noreferrer">
                   LI
@@ -128,7 +160,7 @@ export default function HeroSection() {
       </div>
       <div className="hero_scroll_down">
       <p>scroll</p>
-      {/* <img src={ScrollDownArrow} alt=''/> */}
+      <img src={ScrollDown} alt=''/>
       </div>
     </div>
   </div>
