37df89a89e6c8e0087e7366c84b411dead77d569
Kay <kyv.hardeman@gmail.com>
2021-09-10 21:01:44 -0400

    bug: fixed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ import styled from 'styled-components'
 import Hero from '../assets/images/hero.png'
 
 const HeroStyles = styled.div`
-.hero {
+.hero_section{
   height: 100vh;
   min-height: 1000px;
   width: 100%;
@@ -23,33 +23,52 @@ const HeroStyles = styled.div`
   font-size: 1.5rem;
   margin-bottom: -5.5rem;
   position: relative;
+  color: red;
   span {
     display: inline-block;
     width: 100%;
+    color: red;
   }
   .hero_name {
     font-size: 6rem;
     //TODO change font
     // font-family:
-    color: var(--white)
+    // color: var(--white)
+    color: red;
   }
 }
 .door_img {
-  max-width: 900px;
+  // max-width: 900px;
   width: 100%;
   height: 600px;
   margin: 0 auto;
   border: 2px solid var(--gray-1);
 }
+
+
 .hero_info {
+  color: red;
   margin-top: -20rem;
+
 }
+
+//#FIX ME - I'm overlapping
 .hero_interests,
-.hero_scroll_down{
+.hero_scroll_down {
   display: flex;
   flex-direction: column;
   gap: 2rem;
   position: absolute;
+  color: red;
+  bottom: 20px;
+  width: 50px;
+}
+
+.hero_interests {
+  left: 50px;
+}
+.hero_scroll_down {
+  right: 50px;
 }
 `
 
@@ -57,7 +76,7 @@ const HeroStyles = styled.div`
 export default function HeroSection() {
   return (
     <HeroStyles>
-      <div className='hero'>
+      <div className='hero_section'>
       <div className='container'>
         <h1 className='hero_heading'>
          <span>Hello I'm </span>
@@ -67,16 +86,17 @@ export default function HeroSection() {
             {/* <img src={DoorImg} alt=''/> */}
             <img src={ DoorImg } />
           </div>
-          <div className='hero_info'>
+          <div className="hero_info">
             <ParagraphText> I am a child of PText component - Bonbon cotton candy cupcake cake macaroon. Tootsie roll chocolate cake chocolate muffin liquorice dessert wafer gummies. Donut caramels donut donut tart carrot cake powder carrot cake bonbon.</ParagraphText>
-          </div>
-          <Button btnLink ='/projects' btnText='View My Work 🌸' outline={false}/>
+            <Button btnLink ='/projects' btnText='View My Work 🌸' outline={false}/>
           {/* props created by me: btnLink, btnText, outline - can be named whatever and can have multiple datatypes */}
+          </div>
       <div className='hero_interests'>
           <div className='hero_interests_indicator'>
             <p>customize me</p>
             {/* <img src={InterestsArrow} alt=''/> */}
-            <div className='interests_text'>
+            </div>
+            <div className='hero_interests_text'>
               <ul>
                 <li>
                   {/* linkedin example for now */}
@@ -86,22 +106,25 @@ export default function HeroSection() {
                   rel="noreferrer">
                   LI
                   </a>
+                  </li>
+                  <li>
                   <a href="https://linkedin.com/in/kay-hardeman/"
                   // opens new browser window
                   target='_blank'
                   rel="noreferrer">
                   LI
                   </a>
+                  </li>
+                  <li>
                   <a href="https://linkedin.com/in/kay-hardeman/"
                   // opens new browser window
                   target='_blank'
                   rel="noreferrer">
                   LI
                   </a>
-                </li>
+                  </li>
               </ul>
-            </div>
-        </div>
+          </div>
       </div>
       <div className="hero_scroll_down">
       <p>scroll</p>
