5e6090fb4687cc10d6352e5c38d12a5beef535d7
Kay <kyv.hardeman@gmail.com>
2021-09-12 14:30:25 -0400

    create: individual project  section

---
+++ src/components/IndividualProject.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function IndividualProject() {
+  return (
+    <div>
+      <h1> this is a div</h1>
+    </div>
+  )
+}
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import SectionTitle from './SectionTitle'
+import { Swiper } from "swiper/react";
+import projects from '../assets/data/projects'
 
 export default function ProjectSection() {
   return (
     <div>
       <div className='container'>
         <SectionTitle title="Projects" subtitle="Some of my recent work"/>
+        <div className='project_all_items'>
+          <Swiper>
+            {projects.map((project, index) => {
+              if(index >= 3) return;
+              return
+            })}
+          </Swiper>
+        </div>
       </div>
     </div>
   )
