ff1f29e5528aa3d04c434860ac3d846cd0c642a4
Kay <kyv.hardeman@gmail.com>
2021-09-12 21:39:05 -0400

    feat: swiper css

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import SectionTitle from './SectionTitle'
-import { Swiper } from "swiper/react";
 import projects from '../assets/data/projects'
 import IndividualProject from './IndividualProject';
 import styled from 'styled-components';
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from 'swiper'
+import 'swiper/swiper-bundle.min.css'
+
+SwiperCore.use([Navigation])
 
 const ProjectSectionStyle = styled.div`
 padding: 10rem 0;
@@ -20,6 +25,31 @@ padding: 10rem 0;
   padding-top: 8rem;
   max-width: 100%;
 }
+// swiper arrows
+.swiper-button-prev,
+.swiper-button-next {
+position: absolute;
+height: 50px;
+width: 50px;
+background-color: var(--deep-dark);
+// Note: Z index only works on positioned elements ( position:absolute , position:relative , or position:fixed
+z-index: 10;
+right: 60px;
+left: auto;
+top: 0;
+transform: translateY(50%);
+color: var(--gray-1);
+border-radius: 8px;
+}
+.swiper-button-next {
+  right: 0;
+}
+
+.swiper-button-prev::after,
+.swiper-button-next::after {
+  font-size: 2rem;
+}
+.
 `
 
 export default function ProjectSection() {
@@ -28,10 +58,35 @@ export default function ProjectSection() {
       <div className='container'>
         <SectionTitle title="Projects" subtitle="Some of my recent work"/>
         <div className='project_all_items'>
-          <Swiper>
+          <Swiper
+          spaceBetween={30}
+          slidesPerView={2}
+          navigation
+          breakpoints={
+            {
+              // if window width is better than or equal to 640 show one,
+              640: {
+                slidesPerView: 1,
+              },
+              768: {
+                slidesPerView: 2,
+              },
+              //as I add more projects
+              // 1200: {
+              //   slidesPerView: 3,
+              // }
+
+            }
+          }
+          >
             {projects.map((project, index) => {
               if(index >= 3) return;
-              return <IndividualProject/>
+              return (
+                // unique key from project data id
+                <SwiperSlide key={project.id}>
+                  <IndividualProject/>
+                </SwiperSlide>
+              )
             })}
           </Swiper>
         </div>
