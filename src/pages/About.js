eac4828a6a7ba09128890b12d96ac42ca6e52ff5
Kay <kyv.hardeman@gmail.com>
2021-09-14 06:25:02 -0400

    create: about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,20 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-  <h1>This is the about page</h1>
+      <div className='container'>
+        <div className='top_section'>
+          <div className='left'>
+           <div className='about_subheading'>
+              <p>Hi, I am <span>Kay Hardeman</span>
+              </p>
+                <h2 className='about_heading'></h2>
+                  <div className='about_info'>
+
+                </div>
+           </div>
+         </div>
+       </div>
+      </div>
     </div>
   )
 }
