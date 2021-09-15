9224a2c01255690028f1fabff3ffa5fb51f11602
Kay <kyv.hardeman@gmail.com>
2021-09-15 09:40:07 -0400

    add: project data

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SectionTitle from '../components/SectionTitle'
 import styled from 'styled-components'
-import MdSearch from 'react-icons/md'
+import { MdSearch } from "react-icons/md";
+import IndividualProject from '../components/IndividualProject'
+import ProjectInfo from '../assets/data/projects'
 
 
 const ProjectsStyles = styled.div`
@@ -9,19 +11,31 @@ const ProjectsStyles = styled.div`
 `
 
 export default function Projects() {
+  const [projectData, setProjectData] = useState(ProjectInfo);
+
   return (
-    <ProjectsStyles>
+    <ProjectsStyles >
       <div className='container'>
         <SectionTitle
         title='Projects'
         subtitle='Some of my work'/>
-        <div className="projects_search">
-          <form>
-            <input type='text'>
-              <MdSearch />
-            </input>
-          </form>
 
+        <div className="projects_search_bar">
+          <form>
+            <input type='text'/>
+              <MdSearch />
+          </form>
+        </div>
+
+          <div className='projects_all_items'>
+            {projectData.map(item => (
+                <IndividualProject
+                key={item.id}
+                title={item.name}
+                description={item.desc}
+                img={item.img}
+                />
+            ))}
         </div>
       </div>
     </ProjectsStyles>
