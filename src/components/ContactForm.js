2747b20de88db083cdd903fc495a06219c3f9265
Kay <kyv.hardeman@gmail.com>
2021-09-16 23:40:23 -0400

    feat: form group styling

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,14 +1,53 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
+
+// #TODO contact section
+
+const FormStyle = styled.div`
+width: 100%;
+.form_group {
+  width: 100%;
+  margin-bottom: 2rem;
+  label = font-size: 1.8rem;
+}
+input,
+textarea {
+  width: 100%;
+  font-size: 2rem;
+  padding: 1.2rem;
+  color: var(--gray-1);
+  background-color: var(--deep-dark);
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  margin-top: 1rem;
+}
+textarea {
+  min-height: 250px;
+  resize: vertical;
+}
+
+button[type='submit']{
+  background-color: var(--gray-1);
+  color: var(--black);
+  font-size: 2rem;
+  display: inline-block;
+  border:none;
+  padding: 1rem 4rem;
+  border-radius: 8px;
+  cursor: pointer;
+
+}
+`
+
 
 export default function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
-
-
   return (
-    <div>
+    <FormStyle>
        <form>
           <div className='form_group'>
             <label htmlFor='name'>Your Name
@@ -42,6 +81,6 @@ export default function ContactForm() {
           </div>
           <button type="submit">Send</button>
         </form>
-    </div>
+    </FormStyle>
   )
 }
