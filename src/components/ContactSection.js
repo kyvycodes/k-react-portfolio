34a9102c61156d0f42c320eabb1dde9695f6d919
Kay <kyv.hardeman@gmail.com>
2021-09-16 23:47:01 -0400

    create: contact section

---
+++ src/components/ContactSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SectionTitle from '../components/SectionTitle'
+
+
+export default function ContactSection() {
+  return (
+    <div>
+      <div className='container'>
+        <SectionTitle
+        subtitle='Contact'
+        title='Get In Touch'
+        />
+      </div>
+    </div>
+  )
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
+import ContactSection from '../components/ContactSection'
+
 
 export default function Contacts() {
   return (
     <div>
       <h1>This is the contact page</h1>
       <div>
+        <ContactSection />
         <ContactForm />
         <test />
       </div>
