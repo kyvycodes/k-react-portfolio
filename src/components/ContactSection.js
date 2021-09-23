c837f7fb33a3801665fcb907e093683966f67fc7
Kay <kyv.hardeman@gmail.com>
2021-09-22 23:35:49 -0400

    feat: contact section styling

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -7,7 +7,31 @@ import ContactInfoItem from './ContactInfoItem'
 
 const ContactSectionStyle = styled.div`
 padding: 10rem 0;
-
+.contact_section_wrapper {
+  display: flex;
+  gap: 5rem;
+  margin-top: 7rem;
+  justify-content: space-between;
+  position: relative;
+}
+.contact_section_wrapper::after {
+  position: absolute;
+  content: '';
+  width: 2px;
+  height: 50%;
+  background-color: var(--gary-1);
+  left: 50%;
+  top: 30%;
+  transform: translate(-50%, -50%);
+}
+.left_info {
+  width: 100%;
+  max-width: 500px;
+}
+.right_form {
+  width: 100%;
+  max-width: 500px;
+}
 `
 
 export default function ContactSection() {
