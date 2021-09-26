3d563df4ebd2a9a6b61d428af97eb8c2de16bf7d
Kay <kyv.hardeman@gmail.com>
2021-09-26 01:06:45 -0400

    bug fixed

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -4,43 +4,45 @@ import styled from 'styled-components'
 // BUG not animation not working. Back up - https://dev.to/samwatts98/how-to-easily-animate-your-react-components-on-click-with-css-keyframes-26mg
 
 const CubeAnimation = styled.div`
-:root{
-  --boxColor:#04b7;
-  --rotateSpeed: 30s;
-  --bounceSpeed: 1.5s;
-}
-
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-
-// p {
-// text-align: center;
-// font-size: 30px;
+// :root{
+//   --boxColor:#04b7;
+//   --rotateSpeed: 30s;
+//   --bounceSpeed: 1.5s;
 // }
+
+
+min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    // overflow: hidden;
+    font-size: 75px;
+    perspective: 10em;
+    perspective-origin: 50% calc(50% - 2em);
+
+
+p {
+text-align: center;
+font-size: 25px;
+}
 
 .scene {
   position: relative;
   transform-style: preserve-3d;
-  animation: sceneRotate var(--rotateSpeed) infinite linear;
+  animation: sceneRotate 30s infinite linear;
 
   @keyframes sceneRotate {
       to { transform: rotateY(360deg);}
   }
 }
 .cube {
-  overflow: hidden;
-  perspective: 100em;
-  perspective-origin: 50% calc(50% - 2em);
   position: absolute;
-  width: 20em;
-  height: 20em;
-  bottom: -10em;
-  left: -10em;
+  width: 2em;
+  height: 2em;
+  bottom: -1em;
+  left: -1em;
   transform-style: preserve-3d;
-  // animation: cubeHeight var(--bounceSpeed) infinite ease-out;
+  animation: cubeHeight var(--bounceSpeed) infinite ease-out;
 
   @keyframes cubeHeight {
       0%, 100% { height: 1.5em;}
@@ -51,7 +53,7 @@ const CubeAnimation = styled.div`
       position: absolute;
       width: 100%;
       height: 100%;
-      background: var(--boxColor);
+      background: #04b7;
       box-shadow: 0 0 0.5em #000a inset;
   }
   .front{ transform: translateZ(1em);}
@@ -63,7 +65,7 @@ const CubeAnimation = styled.div`
       position: absolute;
       width: 2em;
       height: 2em;
-      background:var(--boxColor);
+      background:#04b7;
       box-shadow: 0 0 0.5em #000a inset;
       transform: translateY(-50%) rotateX(90deg);
   }
@@ -72,7 +74,7 @@ const CubeAnimation = styled.div`
       position: absolute;
       width: 2em;
       height: 2em;
-      background: var(--boxColor);
+      background: #04b7;
       box-shadow: 0 0 0.5em #000;
       bottom: 0;
       transform: translateY(50%) rotateX(90deg);
@@ -85,7 +87,7 @@ export default function Cube() {
     <CubeAnimation>
       <div className="scene">
          <div className="floor"></div>
-            <div className="cube" id="cube">
+            <div className="cube">
                 <div className="front">
                     <p>Front-end</p>
                 </div>
