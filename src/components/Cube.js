791e7ef7a614b6dced0003e26f54a152888bd0df
Kay <kyv.hardeman@gmail.com>
2021-09-26 12:22:27 -0400

    feat: cube complete

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -4,26 +4,27 @@ import styled from 'styled-components'
 // BUG not animation not working. Back up - https://dev.to/samwatts98/how-to-easily-animate-your-react-components-on-click-with-css-keyframes-26mg
 
 const CubeAnimation = styled.div`
-// :root{
-//   --boxColor:#04b7;
-//   --rotateSpeed: 30s;
-//   --bounceSpeed: 1.5s;
-// }
-
 
 min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+display: flex;
+justify-content: center;
+align-items: center;
     // overflow: hidden;
-    font-size: 75px;
-    perspective: 10em;
-    perspective-origin: 50% calc(50% - 2em);
+font-size: 75px;
+perspective: 10em;
+perspective-origin: 50% calc(50% - 2em);
 
 
 p {
-text-align: center;
+
+// text-align: center;
 font-size: 25px;
+color: white;
+}
+
+.cube_text {
+  text-align: center;
+  top: -10px;
 }
 
 .scene {
@@ -37,45 +38,45 @@ font-size: 25px;
 }
 .cube {
   position: absolute;
-  width: 2em;
-  height: 2em;
-  bottom: -1em;
-  left: -1em;
+  width: 2.2em;
+  height: 2.2em;
+  bottom: -1.1em;
+  left: -1.1em;
   transform-style: preserve-3d;
   animation: cubeHeight var(--bounceSpeed) infinite ease-out;
 
   @keyframes cubeHeight {
-      0%, 100% { height: 1.5em;}
-      8%, 93.5% { height: 2em;}
+      0%, 100% { height: 1.1em;}
+      8%, 93.5% { height: 2.2em;}
   }
 
   .left, .right, .front, .back {
       position: absolute;
       width: 100%;
       height: 100%;
-      background: #04b7;
-      box-shadow: 0 0 0.5em #000a inset;
+      background: #e9bcc1;
+      box-shadow: 0 0 0.9em #e6afb5 inset;
   }
-  .front{ transform: translateZ(1em);}
-  .right{ transform: rotateY(90deg) translateZ(1em);}
-  .back{ transform: rotateY(180deg) translateZ(1em);}
-  .left{ transform: rotateY(270deg) translateZ(1em);}
+  .front{ transform: translateZ(1.1em);}
+  .right{ transform: rotateY(90deg) translateZ(1.1em);}
+  .back{ transform: rotateY(180deg) translateZ(1.1em);}
+  .left{ transform: rotateY(270deg) translateZ(1.1em);}
 
   .top{
       position: absolute;
-      width: 2em;
-      height: 2em;
-      background:#04b7;
-      box-shadow: 0 0 0.5em #000a inset;
+      width: 2.2em;
+      height: 2.2em;
+      background:#e9bcc1;
+      box-shadow: 0 0 1em #e6afb5  inset;
       transform: translateY(-50%) rotateX(90deg);
   }
 
   .bottom{
       position: absolute;
-      width: 2em;
-      height: 2em;
-      background: #04b7;
-      box-shadow: 0 0 0.5em #000;
+      width: 2.2em;
+      height: 2.2em;
+      background: #e9bcc1;
+      box-shadow: 0 0 1em #e6afb5 ;
       bottom: 0;
       transform: translateY(50%) rotateX(90deg);
   }
@@ -86,24 +87,25 @@ export default function Cube() {
   return (
     <CubeAnimation>
       <div className="scene">
-         <div className="floor"></div>
             <div className="cube">
+              <div className='cube_text'>
                 <div className="front">
+                  <br/>
                     <p>Front-end</p>
                 </div>
             <div className="back">
+               <br/>
                   <p>Fullstack</p>
             </div>
             <div className="left">
-              <p>Teacher</p>
+              <p>Mentor</p>
               </div>
             <div className="right">
-              <p>Listener</p>
+              <p>Developer</p>
               </div>
-            <div className="top">
-                 <p>Developer</p>
-            </div>
+            <div className="top"></div>
             <div className="bottom"></div>
+            </div>
         </div>
       </div>
     </CubeAnimation>
