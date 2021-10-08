b10a3b3bc4d08bd971d3f057c5ac4a8828828c30
Kay <kyv.hardeman@gmail.com>
2021-10-08 11:33:02 -0400

    update: resume pdf

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -11,11 +11,11 @@ const projects = [
     id: uuidv4(),
     name: 'Event Share',
     desc:
-      'Planning an event, no matter how big or small can become tedious. Our app facilitates the organization of an event by letting a user manage the logistics with input from guests. The app can be used for any get-together, family reunions, parties, among other activities.',
+      `Planning an event, no matter how big or small can become tedious. Our app facilitates the organization of an event by letting a user manage the logistics with input from guests. The app can be used for any get-together, family reunions, parties, among other activities.`,
     img: EventShare,
     link: 'https://event-share-app.herokuapp.com/',
-    github: 'https://github.com/kyvycodes/event-share-app'
-    // demo:
+    github: 'https://github.com/kyvycodes/event-share-app',
+    // demo: 'https://youtu.be/RbGpbFGiA0A' - input
 
     // features: [
 // 'Javascript',
@@ -36,7 +36,7 @@ const projects = [
     id: uuidv4(),
     name: 'Plantagram',
     desc:
-      'An E-Commerce site that allows guests and users to purchase house plants. Guests can browse subcategories including seasons and living conditions while searching for the new perfect addition to their home.',
+      'An E-Commerce site that allows guests and users to purchase house plants. Guests can browse subcategories including season and living condition while searching for the new perfect addition to their home.',
     img: Plantagram,
     link: 'https://plantagram.herokuapp.com/',
     github: 'https://github.com/Plantagram-App/Plantagram'
